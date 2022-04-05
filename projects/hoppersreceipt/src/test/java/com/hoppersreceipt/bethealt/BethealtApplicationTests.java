000d3a172cebc8e3b0c191efbdb68bf634dc5ad9
bethealt <69316630+bethealt@users.noreply.github.com>
2022-04-05 11:57:43 -0700

    Week Three: Practice Assignment 3

---
+++ projects/hoppersreceipt/src/test/java/com/hoppersreceipt/bethealt/BethealtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoppersreceipt.bethealt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BethealtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
